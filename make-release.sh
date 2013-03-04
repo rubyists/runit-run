309dc82326fec979d7a2e70092ec88b0703cbad2
TJ Vanderpoel <tj@rubyists.com>
2013-03-03 22:02:41 -0600

    added the rest of the files we want excluded

--- make-release.sh
+++ make-release.sh
@@ -5,7 +5,9 @@ tmpdir=$(mktemp -d)
 cd $tmpdir
 git clone $curdir
 checkout=${curdir##*/}
-rm -rf $checkout/.git $checkout/.gitignore $checkout/COPYRIGHT $checkout/README.${pkgname} $checkout/make-release.sh $checkout/${pkgname}.install $checkout/.gitignore
+rm -rf $checkout/.git $checkout/.gitignore $checkout/COPYRIGHT $checkout/README.${pkgname%*-git} \
+       $checkout/make-release.sh $checkout/${pkgname%*-git}.install $checkout/.gitignore \
+       $checkout/PKGBUILD
 tar czvf "${pkgname}-${pkgver}-${pkgrel}.tar.gz" $checkout
 cp "${pkgname}-${pkgver}-${pkgrel}.tar.gz" $curdir
 echo "Wrote ${pkgname}-${pkgver}-${pkgrel}.tar.gz"
