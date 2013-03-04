4ce03288dfdfc444371f8a1c1ad218a93c363a82
TJ Vanderpoel <tj@rubyists.com>
2013-03-03 22:18:49 -0600

    leave some files in place that we use

--- make-release.sh
+++ make-release.sh
@@ -5,9 +5,7 @@ tmpdir=$(mktemp -d)
 cd $tmpdir
 git clone $curdir
 checkout=${curdir##*/}
-rm -rf $checkout/.git $checkout/.gitignore $checkout/COPYRIGHT $checkout/README.${pkgname%*-git} \
-       $checkout/make-release.sh $checkout/${pkgname%*-git}.install $checkout/.gitignore \
-       $checkout/PKGBUILD
+rm -rf $checkout/.git $checkout/make-release.sh $checkout/.gitignore  $checkout/PKGBUILD
 tar czvf "${pkgname}-${pkgver}-${pkgrel}.tar.gz" $checkout
 cp "${pkgname}-${pkgver}-${pkgrel}.tar.gz" $curdir
 echo "Wrote ${pkgname}-${pkgver}-${pkgrel}.tar.gz"
